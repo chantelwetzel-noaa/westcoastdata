4896a67d30c48d076cacf4f81d2286082dbe38c5
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-01-07 14:27:38 -0800

    add survey species list

--- R/get_source.R
+++ R/get_source.R
@@ -1,25 +1,22 @@
 #' Clean up source names
-#'  
+#'
 #' @param data add definition
 #'
 #' @author Chantel Wetzel
 #' @export
 #'
 #'
-get_source <- function(data){
+get_source <- function(data) {
+  use <- unique(data$sources_to_use)
 
-	use <- unique(data$sources_to_use)
-	
-	if("all" %in% use){
-		sources <- c("NWFSC WCGBT", "NWFSC HKL")
-	}
-	if("wcgbt" %in% use){
-		sources <- c("NWFSC WCGBT")		
-	}
-	if("hkl" %in% use){
-		sources <- c("NWFSC HKL")		
-	}
-	return(sources)
+  if ("all" %in% use) {
+    sources <- c("NWFSC WCGBT", "NWFSC HKL")
+  }
+  if ("wcgbt" %in% use) {
+    sources <- c("NWFSC WCGBT")
+  }
+  if ("hkl" %in% use) {
+    sources <- c("NWFSC HKL")
+  }
+  return(sources)
 }
-
-
