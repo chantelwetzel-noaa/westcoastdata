fa208d886e6fa9a79b7155b51c38298d5f27e861
Claire Rosemond <claire.rosemond@noaa.gov>
2026-01-16 12:37:23 -0800

    h&l species names for indices

--- R/get_hkl_species.R
+++ R/get_hkl_species.R
@@ -13,10 +13,12 @@ get_hkl_species <- function() {
     "Cowcod", #  856
     "Greenspotted Rockfish", # 5347
     "Greenstriped Rockfish", #  942
-    "Lingcod", #  938
+#    "Lingcod", #  938
+    "Lingcod South",
     "Squarespot Rockfish", # 1934
     "Starry Rockfish", # 2721
-    "Vermilion Rockfish", #27812
+#    "Vermilion Rockfish", #27812
+    "Vermilion and Sunset Rockfish",
 #    "Yellowtail Rockfish", # 1794
     "Yellowtail Rockfish South"
   )
