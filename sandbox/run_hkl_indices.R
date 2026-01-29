d06ded7c9af5935b52b8fd29eb56395c3a3807ae
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-01-29 07:45:26 -0800

    revise the hkl index code

--- sandbox/run_hkl_indices.R
+++ sandbox/run_hkl_indices.R
@@ -1,96 +1,5 @@
-run_hkl_index <- function(dir = here::here("plots-index"), data) {
-  species_name <- tolower(unique(data$common_name))
-
-  subdata <- data |>
-    dplyr::group_by(year, site_number, drop) |>
-    dplyr::reframe(
-      n = sum(number_caught),
-      depth = median(drop_depth_meters),
-      lat = mean(drop_latitude_degrees),
-      lon = mean(drop_longitude_degrees),
-      effort = length(unique(angler)) * length(unique(hook))
-    ) |>
-    dplyr::mutate(
-      year = as.factor(year),
-      site = as.factor(site_number),
-      drop = as.factor(drop),
-      depth_scaled = (depth - mean(depth)) / sd(depth),
-      depth_scaled_2 = depth_scaled^2
-    )
-
-  # Year and Sites
-  year_site <- expand.grid(
-    year = unique(subdata$year),
-    site_number = unique(subdata$site_number)
-  )
-
-  ## join in location info for all sites
-  locs <- dplyr::group_by(subdata, site_number) |>
-    dplyr::summarise(
-      lat = lat[1],
-      lon = lon[1],
-      drop = drop[1]
-    )
-
-  grid <- dplyr::left_join(year_site, locs) |>
-    dplyr::filter(!is.na(lat + lon))
-
-  # Negative binomial model=============================================
-  fit <- sdmTMB(
-    n ~ 0 + year + site_number + drop,
-    data = subdata,
-    offset = log(subdata$effort),
-    time = "year",
-    spatial = "off",
-    spatiotemporal = "off",
-    family = nbinom2(link = "log")
-  )
-
-  if (fit$pos_def_hessian == TRUE) {
-    index <- calc_index(
-      dir = dir,
-      fit = fit,
-      add_name = paste0(tolower(species_name), '_negbinom'),
-      grid = grid
-    )
-
-    do_diagnostics(
-      dir = dir,
-      add_name = paste0(tolower(species_name), '_negbinom'),
-      fit = fit
-    )
-  }
-
-  # Delta model ================================================================
-  # fit_delta <- sdmTMB(
-  #   n  ~ 0 + year + site_number + drop,
-  #   data = subdata,
-  #   offset = log(subdata$effort),
-  #   time = "year",
-  #   spatial="off",
-  #   spatiotemporal = "off",
-  #   family = delta_gamma()
-  # )
-  #
-  # if(fit_delta$pos_def_hessian == TRUE){
-  #   index <- calc_index(
-  #     dir = dir,
-  #     fit = fit_delta,
-  #     add_name = paste0(species_name, "_delta_gamma"),
-  #     grid = grid)
-  #
-  #   qq_indexwc(
-  #     fit = fit_delta,
-  #     file_name = file.path(dir, paste0(species_name, "_delta_gamma_qq.png"))
-  #   )
-  # }
-}
-
-devtools::load_all()
 library(dplyr)
 library(sdmTMB)
-#library(indexwc)
-#remotes::install_github("pfmc-assessments/indexwc@sap-indices")
 library(ggplot2)
 
 source(here::here("sandbox", "diagnostics.R"))
@@ -102,6 +11,7 @@ source(here::here("sandbox", "match.f.R"))
 source(here::here("sandbox", "plot_betas.R"))
 source(here::here("sandbox", "plot_index.R"))
 source(here::here("sandbox", "plot_qq_indexwc.R"))
+source(here::here("sandbox", "sdmtmb_run_hkl_indices.R"))
 
 species <- get_hkl_species() #should I change this function to include the correct species names?
 #Lingcod to lingcod south, vermilion to vermilion and sunset rockfish, yellowtail rockfish to yellowtail rockfish south
@@ -112,8 +22,9 @@ hkl_data <- read.csv(here::here(
   "nwfsc_hkl_DWarehouse_version_09032025.csv"
 ))
 
-#why can't I used the cleaned version of this
-
+# why can't I used the cleaned version of this
+# The format_hkl_data code expects certain column names and we would need to
+# modify that code in order to use the cleaned data object
 for (sp in unique(species)) {
   species_data <- format_hkl_data(
     common_name = sp,
