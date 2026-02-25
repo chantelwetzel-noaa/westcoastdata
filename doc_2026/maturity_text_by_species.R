dd126d88db9437fae5943b68e6e90e59ed45e0b7
Claire Rosemond <claire.rosemond@noaa.gov>
2026-02-25 13:20:50 -0800

    clean up repository, remove old files

--- doc_2026/maturity_text_by_species.R
+++
@@ -1,9 +0,0 @@
-#mat <- read.csv('C:/Users/Chantel.Wetzel/Documents/GitHub/data_summary/data-raw/maturity_totals.csv')
-mat <- read.csv(file.path(here::here("data-raw", "2026", "maturity_totals.csv")))
-
-maturity_text <- data.frame(
-  species = mat$Species,
-  collected = mat$Count,
-  read = mat$Read,
-  text_to_add = rep("", length(mat$Species))
-)
