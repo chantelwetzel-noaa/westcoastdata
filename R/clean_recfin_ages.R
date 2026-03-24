64449868cf7ace069f36cc343924a43d77e30dce
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-03-24 13:46:18 -0700

    update the recfin cleaning scripts

--- R/clean_recfin_ages.R
+++ R/clean_recfin_ages.R
@@ -1,6 +1,5 @@
 #' Clean RecFIN data
 #'
-#' @param dir Directory location to save the cleaned data frame
 #' @param species add definition
 #' @param data add definition
 #' @param year add definition
@@ -9,73 +8,74 @@
 #' @export
 #'
 #'
-clean_recfin_ages <- function(dir, species, data, year) {
-  # filter down to only unique records with the same SAMPLE_ID
-  # There are 602 duplicate records (394 in OR and 208 in WA)
-  data <- data[which(data$RECFIN_READ_NUMBER %in% c(0, 1)), ]
-
-  data$species_name <- tolower(data$RECFIN_SPECIES_NAME)
-  data$Common_name <- NA
-
-  data <- data[which(data$species_name %in% species[, 1]), ]
-  for (a in 1:dim(species)[1]) {
-    find <- grep(species[a, "name"], data[, "species_name"])
-    data[find, "Common_name"] <- species[a, "use_name"]
-  }
-
-  data$State <- ifelse(
-    data$SAMPLING_AGENCY_NAME == "W",
-    "Washington",
-    ifelse(data$SAMPLING_AGENCY_NAME == "O", "Oregon", NA)
-  )
-  data$Source <- "Recreational"
-  data$State_Source <- paste0(data$Source, "-", data$State)
-  data$State_area <- ifelse(data$State == "Washington", "WA", "OR")
-
-  data$Year <- data$SAMPLE_YEAR
-  data <- data[data$Year >= year, ]
+clean_recfin_ages <- function(species, data, year) {
+  format_data <- data |>
+    dplyr::mutate(
+      Common_name = tolower(RECFIN_SPECIES_NAME)
+    ) |>
+    dplyr::filter(
+      Common_name %in% species[, "use_name"],
+      SAMPLE_YEAR >= year,
+      RECFIN_READ_NUMBER %in% c(0, 1)
+    ) |>
+    dplyr::mutate(
+      State = dplyr::case_when(
+        SAMPLING_AGENCY_NAME == "ODFW" ~ "Oregon",
+        .default = "Washington"
+      ),
+      Source = "Recreational",
+      State_Source = paste0(Source, "-", State),
+      Fleet = RECFIN_MODE_NAME,
+      set_tow_id = 0,
+      Sex = dplyr::case_when(
+        is.na(RECFIN_SEX_CODE) ~ "U",
+        .default = RECFIN_SEX_CODE
+      ),
+      Lengthed = 0,
+      Aged = dplyr::case_when(!is.na(USE_THIS_AGE) ~ 1, .default = 0),
+      Otolith = dplyr::case_when(Aged == 0 ~ 1, .default = 0),
+      Age = dplyr::case_when(
+        !is.na(USE_THIS_AGE) ~ USE_THIS_AGE,
+        .default = NA
+      ),
+      Weight_kg = NA,
+      Length_cm = NA,
+      age_method = NA
+    ) |>
+    dplyr::rename(
+      Year = SAMPLE_YEAR
+    )
 
   # Add yellowtail north
-  yt <- data[data$Common_name == "yellowtail rockfish", ]
-  yt$Common_name <- "yellowtail rockfish north"
-  data <- rbind(data, yt)
+  yt <- which(format_data$Common_name == "yellowtail rockfish")
+  format_data$Common_name[yt] <- "yellowtail rockfish north"
 
-  data$Length_cm <- NA
-  data$Length_cm[which(data$LENGTH_UNITS %in% c("C", ""))] <-
-    data[which(data$LENGTH_UNITS %in% c("C", "")), "MEASURED_LENGTH"]
-  data$Length_cm[which(data$LENGTH_UNITS %in% c("M", "MM"))] <-
-    data[which(data$LENGTH_UNITS %in% c("M", "MM")), "MEASURED_LENGTH"] / 10
+  find <- which(
+    format_data$AGE_READABILITY_DESCRIPTION %in%
+      c(
+        "NOT AGED-PROCESS STORAGE OR COLLECTORS ERROR",
+        "NOT AGED-STRUCTURE NOT DISCERNABLE",
+        "MINIMUM AGE ONLY GIVEN-DIFFICULT TO ASSIGN AGE"
+      )
+  )
+  format_data[find, c("Otolith", "Aged")] <- 0
 
-  data$Lengthed <- 0
-  data$Weight_kg <- NA
+  data_out <- format_data |>
+    dplyr::select(
+      Year,
+      Common_name,
+      State,
+      Source,
+      State_Source,
+      Fleet,
+      set_tow_id,
+      Lengthed,
+      Aged,
+      Otolith,
+      Weight_kg,
+      Length_cm,
+      Age
+    )
 
-  data$Sex <- data$RECFIN_SEX_CODE
-  data$Sex[which(!data$Sex %in% c("F", "M"))] <- "U"
-
-  data$set_tow_id <- 0
-
-  data$Age <- data$USE_THIS_AGE
-  data$Aged <- 0
-  data$Aged[!is.na(data$Age)] <- 1
-
-  data$Otolith <- 0
-  # Oregon does not always record an entry in the RECFIN_STRUCTURE_DESCRIPTION column but
-  # the sample would not be in this table if there was not a structure collected
-  data$Otolith[which(is.na(data$Age))] <- 1
-
-  data[
-    which(
-      data$AGE_READABILITY_DESCRIPTION %in%
-        c(
-          "NOT AGED-PROCESS STORAGE OR COLLECTORS ERROR",
-          "NOT AGED-STRUCTURE NOT DISCERNABLE"
-        ) &
-        !is.na(data$Age)
-    ),
-    "Otolith"
-  ] <- 0
-
-  data$Fleet <- data$RECFIN_MODE_NAME
-
-  return(data)
+  return(data_out)
 }
--- R/clean_recfin_lengths.R
+++ R/clean_recfin_lengths.R
@@ -1,7 +1,6 @@
 #' Clean RecFIN length data
 #'
 #'
-#' @param dir Directory location to save the cleaned data frame
 #' @param or_data add definition
 #' @param wa_data add definition
 #' @param ca_data add definition
@@ -13,124 +12,92 @@
 #'
 #'
 clean_recfin_lengths <- function(
-  dir,
-  or_data = NULL,
-  wa_data = NULL,
-  ca_data = NULL,
+  data,
   species,
   year
 ) {
-  all_data <- rbind(wa_data, or_data, ca_data)
-  all_data$species_name <- tolower(all_data$SPECIES_NAME)
-  all_data$Common_name <- NA
-  #data <- combine_species(data = all_data, name = "Common_name")
-
-  data <- all_data[which(all_data$species_name %in% species[, 1]), ]
-  for (a in 1:dim(species)[1]) {
-    find <- grep(species[a, "name"], data[, "species_name"])
-    data[find, "Common_name"] <- species[a, "use_name"]
+  firstup <- function(x) {
+    substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+    x
   }
 
-  data <- data[which(data$RECFIN_YEAR >= year), ]
-
-  data$State <- tolower(data$STATE_NAME)
-  substr(data$State, 1, 1) <- toupper(substr(data$State, 1, 1))
+  format_data <- data |>
+    dplyr::mutate(
+      Common_name = tolower(SPECIES_NAME)
+    ) |>
+    dplyr::filter(
+      Common_name %in% species[, "use_name"],
+      RECFIN_YEAR >= year,
+      IS_RETAINED == "RETAINED",
+      AGENCY_WATER_AREA_NAME %in%
+        c(
+          "NOT KNOWN", # All Washington records are not know so keeping this across the board
+          "OCEAN",
+          "OCEAN <= 3 MILES",
+          "OCEAN <= 3 MILES (AREAB AND P1B IMPORT)",
+          "OCEAN > 3 MILES",
+          "OCEAN > 3 MILES (AREAB AND P1B IMPORT)"
+        )
+    ) |>
+    dplyr::mutate(
+      State = firstup(tolower(STATE_NAME)),
+      Source = "Recreational",
+      State_Source = paste0(Source, "-", State),
+      Fleet = RECFIN_MODE_NAME,
+      set_tow_id = 0,
+      Sex = dplyr::case_when(
+        !is.na(RECFIN_SEX_CODE) ~ RECFIN_SEX_CODE,
+        .default = "U"
+      ),
+      Length_cm = NA,
+      Weight_kg = 0,
+      Lengthed = dplyr::case_when(!is.na(AGENCY_LENGTH) ~ 1, .default = 0),
+      Age = NA,
+      Aged = 0,
+      Otolith = 0,
+      age_method = NA
+    ) |>
+    dplyr::rename(
+      Year = RECFIN_YEAR
+    )
 
   # Split yellowtail north and south of 40.167
-  yt_north_ca <- data[
+  yellowtail_north <- c(
     which(
-      data$Common_name == "yellowtail rockfish" &
-        data$RECFIN_PORT_NAME ==
+      format_data$Common_name == "yellowtail rockfish" &
+        format_data$RECFIN_PORT_NAME ==
           "WINE (MENDOCINO COUNTY AND SHELTER COVE AREA IN HUMBOLDT COUNTY)"
     ),
-  ]
-  yt_north <- rbind(
-    yt_north_ca,
-    data[
-      which(
-        data$Common_name == "yellowtail rockfish" &
-          data$State %in% c("Oregon", "Washington")
-      ),
-    ]
-  )
-  yt_north$Common_name <- "yellowtail rockfish north"
-
-  yt_south_ca <- data[
     which(
-      data$Common_name == "yellowtail rockfish" &
-        data$State == "California" &
-        data$RECFIN_PORT_NAME !=
-          "WINE (MENDOCINO COUNTY AND SHELTER COVE AREA IN HUMBOLDT COUNTY)"
-    ),
-  ]
-  yt_south_ca$Common_name <- "yellowtail rockfish south"
-
-  data <- rbind(data, yt_north, yt_south_ca)
-  data$State_area <- ifelse(
-    data$State == "Washington",
-    "WA",
-    ifelse(
-      data$State == "Oregon",
-      "OR",
-      ifelse(
-        data$State == "California" &
-          data$RECFIN_PORT_NAME ==
-            "WINE (MENDOCINO COUNTY AND SHELTER COVE AREA IN HUMBOLDT COUNTY)",
-        "NCA",
-        "SCA"
-      )
+      format_data$Common_name == "yellowtail rockfish" &
+        format_data$State %in% c("Oregon", "Washington")
     )
   )
-
-  data$Source <- "Recreational"
-  data$State_Source <- paste0(data$Source, "-", data$State)
-
-  data$Year <- data$RECFIN_YEAR
-
-  keep <- which(
-    data$AGENCY_WATER_AREA_NAME %in%
-      c(
-        "NOT KNOWN", # All Washington records are not know so keeping this across the board
-        "OCEAN",
-        "OCEAN <= 3 MILES",
-        "OCEAN <= 3 MILES (AREAB AND P1B IMPORT)",
-        "OCEAN > 3 MILES",
-        "OCEAN > 3 MILES (AREAB AND P1B IMPORT)"
-      )
+  format_data$Common_name[yellowtail_north] <- "yellowtail rockfish north"
+  yellowtail_south <- which(
+    format_data$Common_name == "yellowtail rockfish" &
+      format_data$RECFIN_PORT_NAME !=
+        "WINE (MENDOCINO COUNTY AND SHELTER COVE AREA IN HUMBOLDT COUNTY)",
+    format_data$State == "California"
   )
-  data <- data[keep, ]
+  format_data$Common_name[yellowtail_south] <- "yellowtail rockfish south"
 
-  remove <- which(is.na(data$RECFIN_LENGTH_MM))
-  if (length(remove) > 0) {
-    data <- data[-remove, ]
-  }
-  remove <- which(data$RECFIN_LENGTH_MM == 0)
-  if (length(remove) > 0) {
-    data <- data[-remove, ]
-  }
-  remove <- which(data$RECFIN_LENGTH_MM > 2000)
-  if (length(remove) > 0) {
-    data <- data[-remove, ]
-  }
+  data_out <- format_data |>
+    dplyr::select(
+      Year,
+      Common_name,
+      State,
+      Source,
+      State_Source,
+      Fleet,
+      set_tow_id,
+      Weight_kg,
+      Lengthed,
+      Aged,
+      Otolith,
+      Length_cm,
+      Age
+    )
 
-  data$Length_cm <- data$RECFIN_LENGTH_MM / 10
-  find <- which(data$AGENCY_LENGTH_UNITS == "C")
-  data$Length_cm[find] <- data$AGENCY_LENGTH[find]
-  data$Lengthed <- 1
-
-  data$Weight_kg <- data$AGENCY_WEIGHT
-  find <- which(data$AGENCY_WEIGHT_UNITS %in% c("G "))
-  data$Weight_kg[find] <- data$AGENCY_WEIGHT[find] / 1000
-
-  data$Sex <- data$RECFIN_SEX_CODE
-  data$Sex[which(!data$Sex %in% c("F", "M"))] <- "U"
-
-  data$set_tow_id <- 0
-  data$Aged <- 0
-  data$Age <- NA
-  data$Otolith <- 0
-
-  data$Fleet <- data$RECFIN_MODE_NAME
-
-  return(data)
+  return(data_out)
 }
