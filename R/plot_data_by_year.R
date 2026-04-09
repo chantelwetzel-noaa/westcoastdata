9ee055f24fe35bbf7d47044715b2a14ccd932ae2
Claire Rosemond <claire.rosemond@noaa.gov>
2026-04-09 10:46:44 -0700

    plots are improving, still need to change legend

--- R/plot_data_by_year.R
+++ R/plot_data_by_year.R
@@ -15,13 +15,13 @@ plot_data_by_year <- function(
   year_range <- min(data$Year):max(data$Year)
   data$Source <- as.factor(data$Source)
   
-  priority_species <- c("Black rockfish", "Bocaccio", "Lingcod north", "Lingcod south", "Longspine thornyhead",
-                        "Pacific spiny dogfish", "Petrale sole", "Redbanded rockfish", "Shortspine thoryhead",
-                        "Widow rockfish", "Yellowtail rockfish south")
-  #remove_sources <- c("CCFRP", "Commercial", "GCDC", "Recreational")
+  priority_species <- c("black rockfish", "bocaccio", "lingcod north", "lingcod south", "longspine thornyhead",
+                        "Pacific spiny dogfish", "petrale sole", "redbanded rockfish", "shortspine thoryhead",
+                        "widow rockfish", "yellowtail rockfish south")
+  remove_sources <- c("CCFRP", "Commercial", "GCDC", "Recreational")
   
-  #data <- dplyr::filter(data,
-  #                           (!Common_name %in% priority_species & !Source %in% remove_sources))
+  data <- dplyr::filter(data,
+                             !(!Common_name %in% priority_species & Source %in% remove_sources))
 
   data_priority <- dplyr::filter(data, Common_name %in% priority_species)
   
