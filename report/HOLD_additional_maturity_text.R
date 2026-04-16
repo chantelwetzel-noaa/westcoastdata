23062af716e050f5ac590f936d37a648eaedb8ae
Claire Rosemond <claire.rosemond@noaa.gov>
2026-04-15 17:41:30 -0700

    added maturity text, but still need to weave it into the report

---
+++ report/HOLD_additional_maturity_text.R
@@ -0,0 +1,32 @@
+#Black rockfish: 
+#Maturity parameter estimates would likely not be updated from those used in the previous assessment.
+
+#Bocaccio: 
+#Maturity parameter estimates may be updated from those used in the previous assessment with additional maturity data and updated modeling methods.
+
+#Lingcod north: 
+#Maturity parameter estimates may be updated from those used in the previous coastwide assessment with additional maturity data and updated modeling methods.
+
+#Lingcod south: 
+#Maturity parameter estimates may be updated from those used in the previous coastwide assessment with additional maturity data and updated modeling methods.
+
+#Longspine thornyhead: 
+#Maturity parameter estimates may be updated from those used in the previous assessment with additional maturity data and updated modeling methods, but assigning maturity status per sample is time consuming for this species.
+
+#Pacific spiny dogfish: 
+#Maturity parameter estimates may be updated from those used in the previous assessment with additional maturity data and updated modeling methods, but assigning maturity status per sample is time consuming for this species.
+
+#Petrale sole: 
+#Maturity parameter estimates may be updated from those used in the previous coastwide assessment with updated modeling methods.
+
+#Redbanded rockfish: 
+#Maturity parameter estimates would be provided using available maturity data and accepted modeling methods.
+
+#Shortspine thornyhead: 
+#Maturity parameter estimates may be updated from those used in the previous assessment with additional maturity data and updated modeling methods.
+
+#Widow rockfish: 
+#Maturity parameter estimates may be updated from those used in the previous assessment with additional maturity data and updated modeling methods.
+
+#Yellowtail rockfish south: 
+#Maturity parameter estimates may be updated from those used in the previous coastwide assessment with additional maturity data and updated modeling methods.
