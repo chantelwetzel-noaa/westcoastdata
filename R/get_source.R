211620cafc99ac6aad2f27bc9e291bfc1e5cf924
Claire Rosemond <claire.rosemond@noaa.gov>
2026-04-17 12:32:03 -0700

    added other sources in survey text for priority species

--- R/get_source.R
+++ R/get_source.R
@@ -9,6 +9,12 @@
 get_source <- function(data) {
   use <- unique(data$sources_to_use)
 
+  if ("all_plus_plus" %in% use) {
+    sources <- c("CCFRP", "Commercial", "GCDC", "NWFSC HKL", "NWFSC WCGBT", "Recreational")
+  }
+  if ("all_plus" %in% use) {
+    sources <- c("CCFRP", "Commercial", "NWFSC HKL", "NWFSC WCGBT", "Recreational")
+  }
   if ("all" %in% use) {
     sources <- c("NWFSC WCGBT", "NWFSC HKL")
   }
@@ -17,6 +23,18 @@ get_source <- function(data) {
   }
   if ("hkl" %in% use) {
     sources <- c("NWFSC HKL")
+  }
+  if ("comm_wcgbt_rec" %in% use) {
+    sources <- c("Commercial", "NWFSC WCGBT", "Recreational")
+  }
+  if ("ccfrp_comm_wcbgt_rec" %in% use) {
+    sources <- c("CCFRP", "Commercial", "NWFSC WCGBT", "Recreational")
+  }
+  if ("comm_wcgbt" %in% use) {
+    sources <- c("Commercial", "NWFSC WCGBT")
+  }
+  if ("ccfrp_comm_hkl_wcgbt" %in% use) {
+    sources <- c("CCFRP", "Commercial", "NWFSC HKL", "NWFSC WCGBT")
   }
   return(sources)
 }
