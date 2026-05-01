cb52076c9d6774a4611da8bb83d318104a7b8c3a
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-05-01 07:54:31 -0700

    edit for clarity

--- report/ongoing_research.R
+++ report/ongoing_research.R
@@ -38,14 +38,16 @@ research_list <- function(species_name) {
       glue::glue(
         "The Cooperative Ageing Program lab has aged 1,468 otoliths collected from commercial fisheries in California 
         between 2019-2024 that were not available for the 2025 update assessment. The large majority of these new ages 
-        are from trawl gear, with 613 from the midwater trawl fishery and 825 from bottom trawl gear. The California midwater trawl ages were collected from Eureka (n = 613) and the bottom trawl ages were collected from 
+        are from trawl gear, with 613 from the midwater trawl fishery and 825 from bottom trawl gear. The California midwater trawl 
+        ages were collected from Eureka (n = 613) and the bottom trawl ages were collected from 
         Eureka (n = 199), Fort Bragg (n = 475), and Moss Landing (n = 151). The California ages from midwater trawl gear range 
         5-28 years with a median (i.e., the middle value in an ordered dataset, separating the higher half from the lower half) 
-        age of 11. These new ages from California are included in the summaries below.
+        age of 11. These new ages (1,468 samples) from California would be available for use in a future
+        assessment and are included in the summaries below.
         
-        These additional ages would be included in a future assessment in combination with the age data included in previous assessments that were collected from trawl fisheries in Oregon 
-        (2,106 from midwater and 274 from bottom trawl) and Washington (4,117 from midwater and 57 from bottom trawl) over the same time period (2019-2024). Ages collected from Oregon and Washington during this time period from midwater trawl gear range from 3-39 years 
-        with a median age of 8 in Oregon and from 1-37 years with a median age of 7 in Washington."
+        For comparison, ages from midwater gear in Oregon and Washington collected during this same period (2019-2024),
+        that were included in the last assessment, range from 3-39 years 
+        with a median age of 8 in Oregon (2,106 ages) and from 1-37 years with a median age of 7 in Washington (4,117 ages)."
       ),
       "\n\n"
     )
